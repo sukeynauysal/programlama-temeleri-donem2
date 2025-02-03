6ab1e0dccca44132449107d0f376ffe00c375f34
sukeynauysal <sukeynauysal05@gmail.com>
2025-02-03 12:08:53 +0300

    Add files via upload

---
+++ odev1.py
@@ -0,0 +1,28 @@
+#ödev 1
+
+gunler=["pazartesi","salı","çarşanba","perşenme","cuma","cumartesi","pazar"]
+
+print(gunler [4])
+     
+
+#ödev2
+ders=["k","o","d","l","m","a"]
+print(ders[0])
+
+ders=["k","o","d","l","m","a"]
+print(ders[2])
+
+ders=["k","o","d","l","m","a"]
+print(ders[5])
+
+
+#ödev3
+ders=["k","o","d","l","m","a"]
+print(ders[-1])
+
+ders=["k","o","d","l","m","a"]
+print(ders[-4])
+
+ders=["k","o","d","l","m","a"]
+print(ders[-3])
+
